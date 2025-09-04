7cd8930b1488b6669fa34bc3d16cd3a3731875f2
Daniel Lúcia <dlucia@refineria.es>
2025-09-04 18:33:39 +0200

    Iniciamos plugin

---
+++ dl-woo-min-purchase.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Plugin Name: Minimum Purchase for WooCoomerce
+ * Description: WooCommerce plugin to configure minimum purchase amounts globally and per category.
+ * Version: 0.0.1
+ * Author: Daniel Lúcia
+ * Author URI: http://www.daniellucia.es
+ * textdomain: dl-woo-min-purchase
+ */
+
+
+defined('ABSPATH') || exit;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use DL\WooMinPurchase\Plugin;
+
+define('DL_WOO_MIN_PURCHASE_VERSION', '0.0.1');
+define('DL_WOO_MIN_PURCHASE_FILE', __FILE__);
+
+add_action('plugins_loaded', function () {
+
+    load_plugin_textdomain('dl-woo-min-purchase', false, dirname(plugin_basename(__FILE__)) . '/languages');
+
+    (new Plugin())->init();
+});
---
+++ src/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DL\WooMinPurchase;
+
+defined('ABSPATH') || exit;
+
+if (! class_exists('WooMinPurchase')) {
+
+    final class Plugin
+    {
+
+        public function init() {}
+    }
+}
